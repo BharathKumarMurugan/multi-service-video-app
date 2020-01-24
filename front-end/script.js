21f83686ea27fac59ec4b15f803b629a4ef31933
Bharath Kumar <mail.bharath92@gmail.com>
2020-01-24 21:32:09 +0530

    updated

--- front-end/script.js
+++ front-end/script.js
@@ -3,16 +3,33 @@ app.controller('myCtrl', function ($scope, $http, filterFilter) {
     $scope.checkbox = {
         value: true
     };
+    var env = {
+        MY_HOST_IP:'18.222.179.155'
+    }
     $scope.checkboxes = [
-        { name: 'git', selected: false },
-        { name: 'jenkins', selected: false },
+        { name: 'git', selected: true },
+        { name: 'jenkins', selected: true },
         { name: 'docker', selected: false },
         { name: 'kubernetes', selected: false },
         { name: 'sonarqube', selected: false },
         { name: 'selenium', selected: false },
         { name: 'aws', selected: false },
         { name: 'azure', selected: false },
-        { name: 'gcp', selected: false }
+        { name: 'gcp', selected: false },
+        { name: 'helm', selected: false },
+        { name: 'subversion', selected: false },
+        { name: 'vagrant', selected: false },
+        { name: 'prometheus', selected: false },
+        { name: 'grafana', selected: false },
+        { name: 'bitbucker', selected: false },
+        { name: 'openshift', selected: false },
+        { name: 'ansible', selected: false },
+        { name: 'spinnaker', selected: false },
+        { name: 'redis', selected: false },
+        { name: 'portainer', selected: false },
+        { name: 'elk', selected: false },
+        { name: 'jenkins x', selected: false },
+        { name: 'nagios', selected: false }
     ]
     $scope.selection = []
     $scope.checkbox_function = function () {
@@ -24,12 +41,12 @@ app.controller('myCtrl', function ($scope, $http, filterFilter) {
         });
     }, true);
 
-    $scope.api = "http://localhost:5000"
+    $scope.api = "http://"+env.MY_HOST_IP+":5000"
     $scope.video_tutorial_list = [];
 
     $http({
         method: 'GET',
-        url: $scope.api
+        url: $scope.api+'/?search=git,jenkins'
     }).then(function successMethod(res) {
         $scope.video_tutorial_list = res.data;
         console.log("Data: " + res.data);
@@ -41,7 +58,7 @@ app.controller('myCtrl', function ($scope, $http, filterFilter) {
         $scope.video_tutorial_list = [];
         console.log(val);
         url = $scope.api+'/?search=' + val
-        alert(url);
+        //alert(url);
         $http({
             method: 'GET',
             url: url
